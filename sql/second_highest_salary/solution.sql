d297a8b721138b5da9252ab9e86d43bdd478fa71
Антонов <vitaly.antonov@altarix.ru>
2026-03-01 20:27:19 +0400

    Second Highest Salary

--- sql/second_highest_salary/solution.sql
+++ sql/second_highest_salary/solution.sql
@@ -1,4 +1,13 @@
 SET search_path TO "second-highest-salary", public;
 
 -- Intentionally left blank: add your solution query locally when solving.
-select DISTINCT * from "second-highest-salary"."Employee" limit 1 OFFSET 1;
+SELECT (SELECT DISTINCT
+	salary
+FROM
+	"Employee" 
+ORDER BY
+	salary DESC
+LIMIT
+	1
+OFFSET
+	1)  AS "SecondHighestSalary";
